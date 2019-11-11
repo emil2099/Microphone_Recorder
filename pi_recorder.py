17e20f9287516bf22229a4a64123536684f1f709
ekalimullin <emil2099@gmail.com>
2019-11-11 17:32:59 +0000

    Add command line args. Change order of 2 mic config.

--- pi_recorder.py
+++ pi_recorder.py
@@ -2,10 +2,21 @@ from mic_threaded import MicArray
 
 import threading
 import time
+import argparse
 
 if __name__ == '__main__':
-    m = MicArray(threshold=50)
-    m.detect_mics()
+
+    parser = argparse.ArgumentParser(description='Raspberry Pi microphone recorder')
+    parser.add_argument("--config", help='Optional json config file to use for microphones')
+    parser.add_argument("--threshold", default=10, help='Detection threshold')
+
+    args = parser.parse_args()
+
+    m = MicArray(threshold=args.threshold)
+    if args.config:
+        m.load_from_file(args.config)
+    else:
+        m.detect_mics()
 
     x = threading.Thread(target=m.record, daemon=True)
     x.start()
