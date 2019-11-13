819c7f6573b1006ca6ed81b612944f8cf0045fac
ekalimullin <emil2099@gmail.com>
2019-11-13 15:53:23 +0000

    add PiJuice

--- pi_recorder.py
+++ pi_recorder.py
@@ -3,6 +3,7 @@ from mic_threaded import MicArray
 import threading
 import time
 import argparse
+import pijuice
 
 if __name__ == '__main__':
 
@@ -13,15 +14,26 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     m = MicArray(threshold=int(args.threshold))
-    if args.config:
-        m.load_from_file(args.config)
-    else:
-        m.detect_mics()
-
+    try:
+        if args.config:
+            m.load_from_file(args.config)
+        else:
+            m.detect_mics()
+    except OSError: # Fix for RPI ALSA issue
+        pass
+    
+    pj = pijuice.PiJuice(1, 0x14)
+    
     time.sleep(0.5)
     x = threading.Thread(target=m.record, daemon=True)
     x.start()
     while True:
         print('Current recording status is: ' + str(m.recording_status))
         print('Current running status is: ' + str(m.running_status))
-        time.sleep(1)
+        if m.recording_status:
+            pj.status.SetLedState('D2', [0, 255,0])
+        elif m.running_status:
+            pj.status.SetLedState('D2', [0,0,255])
+        else:
+            pj.status.SetLedState('D2', [255,0,0])
+        time.sleep(0.5)
